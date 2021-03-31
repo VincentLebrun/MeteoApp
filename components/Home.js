09fc3ca7035bff0810da7445a7412f5475d668f6
Lebrun Vincent <lebrunvincent12@gmail.com>
2021-03-31 16:27:59 +0200

    Start to associate icon to each value weather

--- components/Home.js
+++ components/Home.js
@@ -76,6 +76,80 @@ export default class Home extends Component {
     };
     const myUpdate = new Date(weather.update);
     const formatedDate = Intl.DateTimeFormat("fr-FR", options).format(myUpdate);
+    const findIcon = (i) => {
+      const loading = [
+        {
+          label: "Ensoleillé",
+          icon: "wi-day-sunny",
+          code: 0,
+        },
+        {
+          icon: "wi-day-sunny-overcast",
+          label: "Peu nuageux",
+          code: 1,
+        },
+
+        {
+          icon: "wi-day-fog",
+          label: "Ciel voilé",
+          code: 2,
+        },
+        {
+          icon: "wi-cloud",
+          label: "Nuageux",
+          code: 3,
+        },
+        {
+          icon: "wi-cloudy",
+          label: "Très nuageux",
+          code: 4,
+        },
+        {
+          icon: "wi-smog",
+          label: "Couvert",
+          code: 5,
+        },
+        {
+          icon: "wi-volcano",
+          label: "Brouillard givrant",
+          code: 6,
+        },
+        {
+          icon: "wi-showers",
+          label: "Pluie faible",
+          code: 10,
+        },
+        {
+          icon: "wi-hail",
+          label: "Pluie modérée",
+          code: 11,
+        },
+        {
+          icon: "wi-rain",
+          label: "Pluie forte",
+          code: 12,
+        },
+        {
+          icon: "wi-tsunami",
+          label: "Pluie faible verglaçante",
+          code: 13,
+        },
+        {
+          icon: "wi-tsunami",
+          label: "Pluie faible verglaçante",
+          code: 14,
+        },
+        {
+          icon: "wi-tsunami",
+          label: "Pluie faible verglaçante",
+          code: 15,
+        },
+      ];
+      const myIcon = loading.filter((e) => {
+        return e.code === i;
+      });
+      return myIcon[0].icon;
+    };
 
     return (
       <SafeAreaView>
@@ -108,9 +182,13 @@ export default class Home extends Component {
             </View>
 
             <View style={styles.text}>
-              <Text style={styles.today}>Today</Text>
+              <Text style={styles.today}>Aujourd'hui</Text>
               <View style={styles.middle}>
-                <Icon name="wi-day-sunny" size={80} color="black" />
+                <Icon
+                  name={findIcon(weather.forecast[0].day)}
+                  size={80}
+                  color="black"
+                />
 
                 <Text style={styles.textT}>{weather.forecast[0].tmax}</Text>
               </View>
